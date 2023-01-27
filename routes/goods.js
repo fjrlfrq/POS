21e67eb0c7e47d108acfc23da7f18144ea925ed0
fjrlfrq <fasyarabil21@gmail.com>
2023-01-27 14:42:04 +0700

    POS

--- routes/goods.js
+++ routes/goods.js
@@ -58,7 +58,7 @@ module.exports = function (db) {
         return res.redirect('/goods/add')
       }
 
-      if (!req.files || Object.keys(req.files).length === 0) {
+      if (!req.files || Object.keys(req.files).length === 1) {
         res.status(400).send('No files were uploaded.');
         return;
       }
